55129e019519ce5a9653a73e5b8313611ffa761c
IrinaColgiu <irina.colgiu@gmail.com>
2014-11-12 11:06:34 +0000

    Changed the documentation in the header.

--- tarcheck.py
+++ tarcheck.py
@@ -24,8 +24,8 @@ __author__ = 'ic4'
 #
 #################################################################################
 
-This module has been written for checksumming all the files in a tar archive and comparing
-the checksums against the ones of the original files. In case the md5 sums don't match,
+This module performs the checksums on all the files in a tar archive and compares them
+against the checksums of the original files. In case the md5 sums don't match,
 there will be an error message outputted.
 The checksum on the archive members is done by streaming from the tar the list of members and their metadata,
 and computing the md5 checksum on each member after extracting it.
@@ -33,6 +33,12 @@ and computing the md5 checksum on each member after extracting it.
 Example:
 
 $python tarcheck.py --tar_path /path/to/archive/archive.tar.bz2 --dir /path/to/the/archived/dir
+
+where:
+--tar_path (Required) is the path to the tar archive
+--dir (Required) is the path to the directory that has been archived
+It gives an error if there are files in the archive that can't be found in the directory
+given as input.
 
 It uses < 100MB memory to run.
 
